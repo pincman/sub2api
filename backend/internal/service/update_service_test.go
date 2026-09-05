b6126786389aab659b62fb429f331fab5c146de8
pincman <pincman1988@gmail.com>
2026-09-06 03:52:14 +0800

    test: align custom update tests with direct release updater

--- backend/internal/service/update_service_test.go
+++ backend/internal/service/update_service_test.go
@@ -74,41 +74,6 @@ func TestUpdateServicePerformUpdateNoUpdateReturnsSentinel(t *testing.T) {
 	require.ErrorIs(t, err, ErrNoUpdateAvailable)
 }
 
-type customUpdateDispatcherStub struct {
-	dispatched bool
-	err        error
-}
-
-func (s *customUpdateDispatcherStub) Dispatch(context.Context) error {
-	s.dispatched = true
-	return s.err
-}
-
-func TestUpdateServiceCustomBuildDispatchesOnlyForkReleaseHelper(t *testing.T) {
-	github := &updateServiceGitHubClientStub{
-		release: &GitHubRelease{TagName: "v0.1.160-custom.abc123"},
-	}
-	dispatcher := &customUpdateDispatcherStub{}
-	svc := NewUpdateService(
-		&updateServiceCacheStub{},
-		github,
-		"0.1.159",
-		"custom",
-	)
-	svc.customUpdateDispatcher = dispatcher
-
-	info, err := svc.CheckUpdate(context.Background(), true)
-	require.NoError(t, err)
-	require.True(t, info.HasUpdate)
-	require.Equal(t, customGitHubRepo, github.latestRepo)
-
-	require.ErrorIs(t, svc.PerformUpdate(context.Background()), ErrCustomBuildUpdateDispatched)
-	require.True(t, dispatcher.dispatched)
-
-	require.ErrorIs(t, svc.Rollback(), ErrCustomBuildUpdateDisabled)
-	require.ErrorIs(t, svc.RollbackToVersion(context.Background(), "0.1.158"), ErrCustomBuildUpdateDisabled)
-}
-
 func TestParseVersionHandlesCustomReleaseSuffix(t *testing.T) {
 	require.Equal(t, [3]int{0, 1, 160}, parseVersion("v0.1.160-custom.abc123"))
 	require.Equal(t, [3]int{0, 1, 160}, parseVersion("custom-v0.1.160.abc123"))
